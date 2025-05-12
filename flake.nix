c9eaa6dac16050a2bed32578e0c1052530a1ae09
tktcorporation <tktcorporation.go@gmail.com>
2025-05-12 20:16:21 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -1,50 +1,83 @@
 {
-  description = "Development environment for cross-whisper";
+  description = "A Nix-flake-based Rust development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [rust-overlay.overlays.default self.overlays.default];
+          };
+        });
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = let
+        rust = prev.rust-bin;
       in
-      {
-        # Development shell environment
-        devShells.default = import ./shell.nix {
-          inherit pkgs;
+        if builtins.pathExists ./rust-toolchain.toml
+        then rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain
+        then rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.stable.latest.default.override {
+            extensions = ["rust-src" "rustfmt"];
+          };
+    };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          gobject-introspection
+          cargo
+          cargo-tauri
+          nodejs
+        ];
+
+        buildInputs = with pkgs; [
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
+          openssl
+          libayatana-appindicator
+
+          # coding
+          pnpm
+          rustToolchain
+          rust-analyzer
+          typescript-language-server
+          vscode-extensions.esbenp.prettier-vscode
+        ];
+        env = {
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH";
+          XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
+          WEBKIT_DISABLE_COMPOSITING_MODE = 1;
         };
-        
-        # This allows `nix shell` to work
-        packages.default = pkgs.symlinkJoin {
-          name = "cross-whisper-env";
-          paths = with pkgs; [
-            rustup
-            libiconv
-            nodejs_22
-            jq
-            direnv
-            cargo
-            cargo-tauri
-            nodePackages.pnpm
-            pkg-config
-            gobject-introspection
-            at-spi2-atk
-            atkmm
-            cairo
-            gdk-pixbuf
-            glib
-            gtk3
-            harfbuzz
-            librsvg
-            libsoup_3
-            pango
-            webkitgtk_4_1
-            openssl
-          ];
-        };
-      }
-    );
-} 
+      };
+    });
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,58 +1,27 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+  pkgs = import <nixpkgs> { };
+in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    rustup
-    libiconv
-    nodejs_22
-    jq
-    direnv
-    cargo
-    cargo-tauri
-    nodePackages.pnpm
+  nativeBuildInputs = with pkgs; [
     pkg-config
     gobject-introspection
+    cargo
+    cargo-tauri
+    nodejs
+  ];
+
+  buildInputs = with pkgs;[
     at-spi2-atk
     atkmm
     cairo
-    cairo.dev
     gdk-pixbuf
-    gdk-pixbuf.dev
     glib
-    glib.dev
     gtk3
-    gtk3.dev
     harfbuzz
     librsvg
     libsoup_3
-    libsoup_3.dev
     pango
-    pango.dev
     webkitgtk_4_1
     openssl
   ];
-
-  shellHook = ''
-    export PATH="$HOME/.cargo/bin:$PATH"
-    rustup default stable
-    export PKG_CONFIG_PATH=$(pkg-config --variable pc_path pkg-config):$PKG_CONFIG_PATH
-    
-    # Define shell scripts
-    function test() {
-      cargo test -- --show-output
-    }
-    
-    function start() {
-      cargo run
-    }
-    
-    function build-docs() {
-      cargo doc
-    }
-    
-    # Export the functions
-    export -f test
-    export -f start
-    export -f build-docs
-  '';
-} 
+}
